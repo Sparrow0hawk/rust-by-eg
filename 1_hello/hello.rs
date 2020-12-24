729ea64f7cdeddc5a2039681ebcb7cff6bbfc31f
sparrow0hawk <alexjcoleman@gmail.com>
2020-12-24 14:52:48 +0000

    initial commit and hello world

---
+++ 1_hello/hello.rs
@@ -0,0 +1,6 @@
+// this is a comment
+fn main() {
+	// statements here are excuted when compiled binary is installed
+	println!("Hello world!");
+	println!("I'm a Rustacean!");
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Rust by example
+
+Working through [rust by examples](https://doc.rust-lang.org/rust-by-example/index.html) exercises.
