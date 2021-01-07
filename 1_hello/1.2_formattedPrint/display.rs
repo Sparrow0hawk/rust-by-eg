d2ac24f342b55ea251c2aa9afb3c5de90955efd3
sparrow0hawk <alexjcoleman@gmail.com>
2021-01-07 07:52:17 +0000

    implementing my own display for a type

--- 1_hello/1.2_formattedPrint/display.rs
+++ 1_hello/1.2_formattedPrint/display.rs
@@ -4,9 +4,20 @@ use std::fmt;
 #[derive(Debug)]
 struct TipTop(i32, i32);
 
+// to use {} marker  need to manually implement the fmt:display  for type
+
+impl fmt::Display for TipTop {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // write the first element to output stream f
+        // returns a result which determines success or failure
+        write!(f, "{}", self.0)
+    }
+}
+
 fn main() {
     let tiptop = TipTop(10,11);
 
     //println!("Display: {}", tiptop);
     println!("Debug: {:?}", tiptop);
+    println!("Display {}", tiptop);
 }
